5efe0ad47dddf1067d7ad7ce211cdd711043e344
hiro712 <61372263+hiro712@users.noreply.github.com>
2021-09-01 08:09:29 +0900

    Update view.php

--- view.php
+++ view.php
@@ -180,6 +180,7 @@ $stmt_->execute($params_);
                 </p>
 
                 <div class="infomation">
+		    <p>ニュース/お知らせなど</p>
                     <iframe src="info.html" width="300" height="650"></iframe>
                 </div>
 
