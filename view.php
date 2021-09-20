3f554ddaad255e613e46ec183729c304459b99ea
hiro712 <61372263+hiro712@users.noreply.github.com>
2021-09-20 23:53:06 +0900

    Update view.php

--- view.php
+++ view.php
@@ -343,7 +343,7 @@ $stmt_->execute($params_);
         <footer>
             <div class="menu">
                 <ul>
-                    <li>    &copy; 2021 Hiroto ABE, Tohoku University Inc. All Rights Reserved.   Ver.1.2.2 </li>
+                    <li>    &copy; 2021 Hiroto ABE, Tohoku University Inc. All Rights Reserved.   Ver.1.3.0 </li>
                 </ul>
             </div>
         </footer>
