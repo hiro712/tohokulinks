ccd9cf2a66fb308284f26a86ac84b6f0e9dd7ad9
hiro712 <61372263+hiro712@users.noreply.github.com>
2021-09-28 00:08:47 +0900

    Update view.php

--- view.php
+++ view.php
@@ -177,7 +177,7 @@ $stmt_->execute($params_);
                     下にスクロールしてご利用ください。<br>
 		</p>
                     <a class="back" href="account.html">アカウント管理</a>
-                    <a class="back" href="https://forms.gle/KUUAUuDbdFBCkus49">お問い合わせ</a>
+                    <a class="back" href="form.html">お問い合わせ</a>
 		　　　　　　　　<a class="back" href="manual.pdf">使い方</a>
 		    <a class="enews" href="info.html">お知らせ</a>
 	　　　　　　　　　　　　　　　　　　　　　　　　
